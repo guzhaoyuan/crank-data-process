34ff068de59466573a7b404f4e2bf8a063dd51ce
guzhaoyuan <gu-zy14@mails.tsinghua.edu.cn>
2018-04-22 12:41:04 +0800

    fft and filter done, now process data

--- dataRead.m
+++ dataRead.m
@@ -9,25 +9,30 @@ n = find(a<114,1,'last');
 
 figure;
 % subplot(2,1,1);plot(A1(:,1),A1(:,2));
-subplot(2,2,1);plot(A1(m:n,1),A1(m:n,2));
+subplot(2,3,1);plot(A1(m:n,1),A1(m:n,2));
 %% filter for power
 fs = 5000;
-fc=100;
+fc=200;
 wn = fc/(fs/2);
-[b,a]=butter(4,wn,'high');
-y_high=filter(b,a,y);
-subplot(2,2,2);plot(A1(m:n,1),y_high);
+[b,a]=butter(4,wn,'low');
+y_power_high=filter(b,a,A1(m:n,2));
+subplot(2,3,2);plot(A1(m:n,1),y_power_high);
+
+%% fft for power
+
+y_power_fft = fft(A1(m:n,2));
+subplot(2,3,3);plot(A1(m:n,1),y_power_fft);
 %% read outline data from txt
 
 A2=textread('H15TCER.413');
 A2=A2(2:end,2:end);
 A2=A2(:);
-subplot(2,2,3);plot([1:size(A2,1)],A2);
+subplot(2,3,4);plot([1:size(A2,1)],A2);
 
 %% filter for outline
-fs = 5000;
-fc=100;
+fs = 3600;
+fc=200;
 wn = fc/(fs/2);
-[b,a]=butter(4,wn,'high');
-y_high=filter(b,a,y);
-subplot(2,2,2);plot(A1(m:n,1),y_high);
+[b,a]=butter(4,wn,'low');
+y_outline_high=filter(b,a,A2);
+subplot(2,3,5);plot([1:size(A2,1)],y_outline_high);
