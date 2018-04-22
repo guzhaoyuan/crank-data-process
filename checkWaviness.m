1a0ee5f2627f76b20d5c20e4f356ea17701db457
guzhaoyuan <gu-zy14@mails.tsinghua.edu.cn>
2018-04-23 02:33:34 +0800

    change file name and reason structure

--- checkWaviness.m
+++ checkWaviness.m
@@ -1,5 +1,5 @@
 clc;
-%% init data
+%% init outline data
 A2=textread('H15TCER.413');
 A2=A2(2:end,2:end);
 A2=A2(:);
@@ -9,7 +9,10 @@ N=3600;
 n=0:N-1;
 m=1:N/2;
 f=m*Fs/N;
+%% settle filter param & apply band filter
+% fc1=200; fc2=400;
 
+%% fft outline and plot
 Y = fft(A2);
 Y=Y(1:1800)
 plot(f,abs(Y));
