a790625e2a251747bf84023bec9d1c6948b7a17a
guzhaoyuan <gu-zy14@mails.tsinghua.edu.cn>
2018-04-23 20:16:25 +0800

    增加了plot功率的程序

---
+++ plot_power.m
@@ -0,0 +1,12 @@
+close all;clc;%clear;
+%% read power data from excel
+% A1=xlsread('16TH.xlsx');
+a = A1(:,1);
+% data every 0.2ms
+
+m = find(a>85,1,'first');
+n = find(a<107,1,'last');
+
+figure;
+% plot(A1(:,1),A1(:,2));
+plot(A1(m:n,1),A1(m:n,2));title('power data');ylabel('功率');xlabel('时间/s');
